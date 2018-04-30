dc7172aa01fb6689848038f134e39b7dd24439fc
Paul <pcherian10@gmail.com>
2018-04-30 18:59:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,14 @@
 
 def countdown (number)
   while(number > 0)  
-    "#{number} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
 end
 
 def countdown_with_sleep (number)
   while(number > 0)  
-    "#{number} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
        sleep(1.seconds)
     number -= 1
   end
